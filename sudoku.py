3f0c5bdbeb3a11de4acaaf848bfc648642bb860b
Subhashis Hansda <subhashishansda2@gmail.com>
2022-03-03 22:11:19 +0530

    creating board for displaying numbers

---
+++ sudoku.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  3 21:38:12 2022
+
+@author: VAGUE
+"""
+
+# printing the board for displaying the numbers
+def print_board(puzzle):
+    for r in range(len(puzzle)):
+        if r%3 == 0 and r != 0:
+            print("- - - - - - - - - - - -")
+        
+        for c in range(len(puzzle[0])):
+            if c%3 == 0 and c != 0:
+                print(" | ", end="")
+                
+            if c == 8:
+                print(puzzle[r][c])
+            else:
+                print(str(puzzle[r][c]) + " ", end="")
+                
