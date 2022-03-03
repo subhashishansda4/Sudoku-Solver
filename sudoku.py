ef3cde592e8284690284725982d87c2dedd736af
Subhashis Hansda <subhashishansda2@gmail.com>
2022-03-03 22:36:50 +0530

    checking sudoku rules

--- sudoku.py
+++ sudoku.py
@@ -27,3 +27,25 @@ def find_empty(puzzle):
                 return r, c
     
     return None, None
+
+# checking sudoku rules
+def valid(puzzle, guess, pos):
+    # for checking every row
+    for i in range(len(puzzle[0])):
+        if puzzle[pos[0]][i] == guess and pos[1] != i:
+            return False
+     
+    # for checking every column
+    for i in range(len(puzzle)):
+        if(puzzle[i][pos[1]]) == guess and pos[0] != i:
+            return False
+         
+    # for checking every cube box
+    cube_x = pos[1] // 3
+    cube_y = pos[0] // 3
+    for i in range(cube_y*3, cube_y*3+3):
+        for j in range(cube_x*3, cube_x*3+3):
+            if puzzle[i][j] == guess and (i,j) != pos:
+                return False
+     
+    return True
